d40bbc2418aa563fd9a664d2e00067b3d39b7190
jeongii <hyenj629@naver.com>
2026-09-18 17:36:06 +0900

    0025

--- src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -1,6 +1,7 @@
 package com.back.boundedContext.cash.in;
 
 import com.back.boundedContext.cash.app.CashFacade;
+import com.back.boundedContext.cash.domain.CashMember;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.post.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,8 @@ public class CashEventListener {
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberModifiedEvent event) {
-        cashFacade.syncMember(event.getMember());
+        CashMember member = cashFacade.syncMember(event.getMember());
+
+        cashFacade.createWallet(member);
     }
 }
