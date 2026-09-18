72f731245f9286fd85a8984a477549a0ec5cf181
jeongii <hyenj629@naver.com>
2026-09-18 17:36:06 +0900

    0024, 0025 CashMember와 1:1 관계인 지갑 생성, user1 회원의 지갑에 15만원, 10만원, 5만원 입금, user2 회원의 지갑에 15만원 입금

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -1,16 +1,21 @@
 package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.domain.Wallet;
 import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.boundedContext.cash.out.WalletRepository;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CashFacade {
     private final CashMemberRepository cashMemberRepository;
+    private final WalletRepository walletRepository;
 
     @Transactional
     public CashMember syncMember(MemberDto member) {
@@ -25,5 +30,22 @@ public class CashFacade {
         );
 
         return cashMemberRepository.save(_member);
+    }
+
+    @Transactional
+    public Wallet createWallet(CashMember holder) {
+        Wallet wallet = new Wallet(holder);
+
+        return walletRepository.save(wallet);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<CashMember> findMemberByUsername(String username) {
+        return cashMemberRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Wallet> findWalletByHolder(CashMember holder) {
+        return walletRepository.findByHolder(holder);
     }
 }
